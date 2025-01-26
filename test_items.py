27ba67a486ff826882713be26ef10967ed8406e4
Anton <gorkov.anton123@gmail.com>
2025-01-26 15:44:14 +0300

    Изменение assert и вариант его правильной работы

--- test_items.py
+++ test_items.py
@@ -9,7 +9,7 @@ class Test_lang():
         link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
         browser.get(link)
         time.sleep(5)
-        button = browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
-        assert button
+        button = browser.find_elements(By.CSS_SELECTOR, '.btn-add-to-basket')
+        assert button, "Элемента кнопки добавления в корзину на странице нет"
 
 
